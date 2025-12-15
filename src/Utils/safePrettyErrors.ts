468a5851392dda7c45edb342481f5d327d0411e9
Sants <106097059+CardonaSantos@users.noreply.github.com>
2025-12-15 15:05:02 -0600

    ajuste pretty

--- src/Utils/safePrettyErrors.ts
+++ src/Utils/safePrettyErrors.ts
@@ -1,10 +1,13 @@
-function safePretty(obj: any, maxChars = 2000) {
+const MAX_LOG_CHARS = 25_000;
+
+export function safePretty(obj: any, maxChars = MAX_LOG_CHARS) {
   let s: string;
+
   try {
     const json = JSON.stringify(obj, null, 2);
-    s = typeof json === 'string' ? json : String(json); // "undefined"
-  } catch (e) {
-    return `[unstringifiable] ${(e as any)?.message ?? e}`;
+    s = typeof json === 'string' ? json : String(json);
+  } catch (e: any) {
+    return `[unstringifiable] ${e?.message ?? e}`;
   }
 
   if (s.length <= maxChars) return s;
